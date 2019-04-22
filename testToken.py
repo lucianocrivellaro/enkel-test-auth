caf42f4a979f6d27fbeea56911e819f1846385cd
Cesar Maia <cesar.maia@enkel.com.br>
2019-04-22 17:10:38 -0300

    Add url param try/except add print infos

--- testToken.py
+++ testToken.py
@@ -3,6 +3,7 @@
 import requests, argparse, json
 
 parser = argparse.ArgumentParser()
+parser.add_argument('-url', dest='oamUrl', help='URL do OAM', required=True)
 parser.add_argument('-u', dest='userName', help='user name to auth', required=True)
 parser.add_argument('-p', dest='passWord', help='password to auth', required=True)
 parser.add_argument('-tu', dest='oamTokenUser', help='oam token user', required=True)
@@ -11,27 +12,37 @@ parser.add_argument('-vu', dest='oamValidatorUser', help='oam validator user', r
 parser.add_argument('-vp', dest='oamValidatorPass', help='oam validator password', required=True)
 args = parser.parse_args()
 
-oamUrl = 'https://oam.ppd.veloe.com.br'
-
 getTokenRequest = {
-	'grant_type': 'password',
-	'username': args.userName,
-	'password': args.passWord,
-	'scope': 'primeiro-acesso.insert.credenciais'
+    'grant_type': 'password',
+    'username': args.userName,
+    'password': args.passWord,
+    'scope': 'primeiro-acesso.insert.credenciais'
 }
-token = requests.post(oamUrl + '/ms_oauth/oauth2/endpoints/oauthservice/tokens', data=getTokenRequest, auth=(args.oamTokenUser, args.oamTokenPass), verify=False)
+try:
+    print('Pegando o token de acesso')
+    token = requests.post(args.oamUrl + '/ms_oauth/oauth2/endpoints/oauthservice/tokens', data=getTokenRequest, auth=(args.oamTokenUser, args.oamTokenPass), verify=False)
+    pass
+except Exception as e:
+    raise e
+print('Token recebido')
 # print(json.loads(token.text)['access_token'])
 
 validateRequest = {
-	'grant_type': 'oracle-idm:/oauth/grant-type/resource-access-token/jwt',
-	'oracle_token_action': 'validate',
-	'scope': 'primeiro-acesso.insert.credenciais',
-	'assertion': json.loads(token.text)['access_token']
+    'grant_type': 'oracle-idm:/oauth/grant-type/resource-access-token/jwt',
+    'oracle_token_action': 'validate',
+    'scope': 'primeiro-acesso.insert.credenciais',
+    'assertion': json.loads(token.text)['access_token']
 }
+
+print('Fazendo 100 chamadas de validação...')
 
 count = 0
 while count < 100:
-	resultValidator = requests.post(oamUrl + '/ms_oauth/oauth2/endpoints/oauthservice/tokens', data=validateRequest, auth=(args.oamValidatorUser, args.oamValidatorPass), verify=False)
-	print(resultValidator.text)
-	count += 1
+    try:
+        resultValidator = requests.post(args.oamUrl + '/ms_oauth/oauth2/endpoints/oauthservice/tokens', data=validateRequest, auth=(args.oamValidatorUser, args.oamValidatorPass), verify=False)
+        print(resultValidator.text)
+        pass
+    except Exception as e:
+        raise e
+    count += 1
 
