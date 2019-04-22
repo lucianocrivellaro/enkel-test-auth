22c119cfa9baa43916c148ee0a700f8e93df2460
Cesar Maia <cesar.maia@enkel.com.br>
2019-04-22 17:04:53 -0300

    Fix load json

--- testToken.py
+++ testToken.py
@@ -1,6 +1,6 @@
 #!python
 
-import requests, argparse
+import requests, argparse, json
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-u', dest='userName', help='user name to auth', required=True)
@@ -20,13 +20,13 @@ getTokenRequest = {
 	'scope': 'primeiro-acesso.insert.credenciais'
 }
 token = requests.post(oamUrl + '/ms_oauth/oauth2/endpoints/oauthservice/tokens', data=getTokenRequest, auth=(args.oamTokenUser, args.oamTokenPass), verify=False)
-print(token.text.json()['access_token'])
+print(json.loads(token.text)['access_token'])
 
 validateRequest = {
 	'grant_type': 'oracle-idm:/oauth/grant-type/resource-access-token/jwt',
 	'oracle_token_action': 'validate',
 	'scope': 'primeiro-acesso.insert.credenciais',
-	'assertion': token.text.json()['access_token']
+	'assertion': json.loads(token.text)['access_token']
 }
 resultValidator = requests.post(oamUrl + '/ms_oauth/oauth2/endpoints/oauthservice/tokens', data=validateRequest, auth=(args.oamValidatorUser, args.oamValidatorPass), verify=False)
 
